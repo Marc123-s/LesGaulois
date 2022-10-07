7c61a1f56a81cad1214589ab2e9f64861a69c376
sakr marc <skm4139a@ens.fsi>
2022-10-07 08:26:55 +0200

    Debut TP1

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,38 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte ) {
+		System.out.println(prendreParole() + "«" + texte + "»" );
+		
+		
+	}
+	private String prendreParole() {
+		// TODO Auto-generated method stub
+		return "Le gaulois" + "non" + " : ";
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+
+	
+}
